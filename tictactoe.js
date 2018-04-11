5082296f8e0a56632377a82d38f4fd2352016066
Michael <mqngo21@gmail.com>
2018-04-11 14:22:34 -0700

    created player turn function

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,24 @@
 let board = [];
+let turn = true;
+let player = null;
+let result = false;
+
+const playerTurn = () => {
+  for (var i = 0; i < 9; i++) {
+    if (result === false) {
+      turn = !turn;
+    } else {
+      console.log('Player ' +  player + 'won!');
+    }
+    if (turn === false) {
+      player = 'X';
+      console.log('It is player X turn!');
+    } else {
+      player = 'O';
+      console.log('It is player O turn!');
+    }
+  }
+}
 
 const makeBoard = () => {
 
@@ -14,6 +34,18 @@ const makeBoard = () => {
   return board;
 }
 
+const move = (num) => {
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      if (num === 1) {
+        board[0][0] = 'X';
+      }
+    }
+  }
+
+  return board;
+}
+
+playerTurn();
 makeBoard()
 console.log(board);
-
